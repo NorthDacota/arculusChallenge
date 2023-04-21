e07f8b86c3ce58109ea94bfa17a15c60710b51e8
Andrei Egorov <egorow777@gmail.com>
2023-04-21 18:01:31 +0200

    argparser and pending pipelines detecter were added

--- pipelineCheck.py
+++ pipelineCheck.py
@@ -7,109 +7,62 @@ import getopt
 import sys
 import gitlab
 from pathlib import Path
+import argparse
 
-token_file = "./token"
-default_gitlab_host = 'http://localhost/'
-gitlab_host = ''
-duration_limit = 600
-default_project_name = 'gitlab-instance-831d1df5/mytestproject'
-project_name = ''
-fetch_logs = False
-token = ''
 unic_pipelines = []
-report_flag = False
-verbose = False
-trigger_failed = False
 
+parser = argparse.ArgumentParser(description='\nUse arguments or default variables will be set\n'
+          'The script shows statuses of the last pipelines in every branch of a project\n'
+          'Don\'t forget specify a project name and a host of gitlab\n'
+          'Please put your token in file ./token_file in the same directory or use the \"token\" key '
+          '(The first option should safe if you set 600 on it)')
 
-###
-# Print help for CLI
-def usage():
-    print("\nUse arguments or default variables will be set\n"
-          "The script shows statuses of the last pipelines in every branch of a project\n"
-          "Don't forget specify a project name and a host of gitlab\n"
-          "Please put your token in file ./token_file in the same directory or use the \"token\" key "
-          "(The first option should safe if you set 600 on it)\n")
-    print("--gitlab_host=\n"
-          "    Specify your GitLab host. Default value - localhost\n"
-          "--project_name=\n"
-          "    Specify your project name.\n"
-          "--token=\n"
-          "    Set token\n"
-          "--report-problems\n"
-          "    Send an issue about slow or failed jobs in the last pipelines\n"
-          "--trigger-failed\n"
-          "    Trigger all failed jobs in the last pipelines\n"
-          "--duration_limit=\n"
-          "    Any running job will be considered slow if it exceeds this limit. Default value - 600 seconds.\n"
-          "--fetch-logs\n"
-          "    Get logs from all failed jobs in the last pipelines. They will be stored in files of a current director"
-          "y")
+parser.add_argument('--gitlab_host', dest='gitlab_host', type=str, default="http://localhost/",
+                    help='Specify your GitLab host. Default value - localhost')
 
-    print("\n")
+parser.add_argument('--token', type=str, default='', help='Set token')
 
+parser.add_argument('--project_name', type=str, default="gitlab-instance-831d1df5/mytestproject",
+                    help='Specify your project name.')
 
-###
-# CLI options
-try:
-    opts, args = getopt.getopt(sys.argv[1:], "ho:v", ["help", "token=", "trigger-failed",
-                                                      "duration_limit=", "fetch-logs", "gitlab_host=", "project_name=",
-                                                      "report-problems"])
-except getopt.GetoptError as err:
-    print(err)
-    usage()
-    sys.exit(2)
+parser.add_argument('--duration_limit', type=int, default=600,
+                    help='Any running job will be considered slow if it exceeds this limit. '
+                         'Default value - 600 seconds.')
 
-for o, a in opts:
-    if o in ("-h", "--help"):
-        usage()
-        sys.exit()
-    elif o in "--token":
-        token = str(a)
-    elif o in "--fetch-logs":
-        fetch_logs = True
-    elif o in "--gitlab_host":
-        gitlab_host = str(a)
-    elif o in "--project_name":
-        project_name = str(a)
-    elif o in "--report-problems":
-        report_flag = True
-    elif o in ("-v", "--verbose"):
-        verbose = True
-    elif o in "--trigger-failed":
-        trigger_failed = True
-    elif o in "--duration_limit":
-        duration_limit = int(a)
+parser.add_argument('--report-problems', dest="report_flag", action='store_true', default=False,
+                    help='Send an issue about slow or failed jobs in the last pipelines')
+
+parser.add_argument('--fetch-logs', dest="fetch_logs", action='store_true', default=False,
+                    help='Get logs from all failed jobs in the last pipelines. '
+                         'They will be stored in files of a current directory')
+
+parser.add_argument('--trigger-failed', dest="trigger_failed", action='store_true', default=False,
+                    help='Trigger all failed jobs in the last pipelines')
+
+parser.add_argument('--token_file', dest="token_file", type=str, default="./token_file",
+                    help='Specify a token file.')
+
+args = parser.parse_args()
 
 # check if a token exists
-if token == '':
+if args.token == '':
     try:
-        token = Path('./token_file').read_text().replace('\n', '').replace('b', '')
+        args.token = Path(args.token_file).read_text().replace('\n', '').replace('b', '')
     except Exception as e:
         print(f'Unable to get token from file.. Use --token or create token_file. Error: {e}')
         exit(1)
 
-# Use default value if a host wasn't set
-if gitlab_host == '':
-    gitlab_host = default_gitlab_host
-    print("Using default host: ", gitlab_host)
-
-# Check if a project was set
-if project_name == '':
-    project_name = default_project_name
-    print('Using default project name: ', project_name)
-
 # Connect to GitLab
 try:
-    gl = gitlab.Gitlab(gitlab_host, private_token=token)
-    my_project = gl.projects.get(project_name)
+    gl = gitlab.Gitlab(args.gitlab_host, private_token=args.token)
+    my_project = gl.projects.get(args.project_name)
 except Exception as e:
     print(f"I can't connect to the gitlab or get access to the project. "
-          f"Check \"gitlab_host\", \"project_name\" and \"token\" params. Error:\n {e}")
+          f"Check \"--gitlab-host\", \"--project-name\" and \"--token\" params. Error:\n {e}")
     sys.exit(1)
 
 # Check if the project is empty
-pipelines = my_project.pipelines.list()
+pipelines = my_project.pipelines.list(get_all=True)
 if len(pipelines) == 0:
     print("There are no pipelines in the project")
     exit(0)
@@ -117,7 +70,7 @@ if len(pipelines) == 0:
 
 # Send an issue in GitLab
 def report_problem(message, description):
-    if report_flag:
+    if args.report_flag:
         title = 'Job ' + str(message)
         report_to_project = my_project.issues.create({'title': title, 'description': description})
         print("Problem has been reported with the title: \n" + title)
@@ -151,7 +104,7 @@ def get_trace(id):
     last_log = log_data.split('\n')
     separator = "\n    "
     print("    TRACE:\n    ", separator.join(last_log[-5:]))
-    if fetch_logs:
+    if args.fetch_logs:
         filename = str(thejob.id) + '.log'
         logfile = open(filename, "w")
         logfile.write(log_data)
@@ -176,6 +129,20 @@ def slow_jobs_handler(job):
     description = str(job.id) + " " + job.attributes['web_url']
     report_problem(message, description)
 
+def slow_jobs_handler(job, pending=False):
+    print_job_stat(job)
+    if pending:
+        duration_attribute = 'queued_duration'
+        job_status_for_message = " pending too long"
+    else:
+        duration_attribute = 'duration'
+        job_status_for_message = " too slow"
+    print("    The job is",job_status_for_message,': ', str(job.attributes[duration_attribute]) + "sec")
+    get_trace(job.id)
+    message = str(job.name) + ' at ' + str(job.stage) + " stage is" +\
+              job_status_for_message + ". Duration: " + str(job.attributes[duration_attribute])
+    description = str(job.id) + " " + job.attributes['web_url']
+    report_problem(message, description)
 
 # It takes only the last pipeline in each branch and if the pipeline has a problem makes a report
 for pipeline in pipelines:
@@ -188,9 +155,11 @@ for pipeline in pipelines:
             for job in jobs:
                 if job.status == "failed":
                     failed_jobs_handler(job)
-                if job.status == "running" and job.attributes['duration'] >= duration_limit:
+                if job.status == "running" and job.attributes['duration'] >= args.duration_limit:
                     slow_jobs_handler(job)
-            if trigger_failed: pipeline.retry()
+                if job.status == "pending" and job.attributes['queued_duration'] >= args.duration_limit:
+                    slow_jobs_handler(job, pending=True)
+            if args.trigger_failed: pipeline.retry()
 
     else:
         continue
